15de7e9ce437c9645478cd0b56b7196fdf5ea02d
Gabe Berke-Williams <gbw@brandeis.edu>
2010-06-09 18:15:31 -0400

    Add map.root => episodes controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
+  map.root :controller => 'episodes'
   map.resources :episodes
 
   # The priority is based upon order of creation: first created -> highest priority.
