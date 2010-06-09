c88a3a0a88060c7bf65daaa1bc6a5ced3e92435b
Gabe Berke-Williams <gbw@brandeis.edu>
2010-06-09 19:33:40 -0400

    Add show_id to episodes

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100609221924) do
+ActiveRecord::Schema.define(:version => 20100609232824) do
 
   create_table "episodes", :force => true do |t|
     t.string   "name"
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(:version => 20100609221924) do
     t.integer  "number"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "show_id"
   end
 
   create_table "shows", :force => true do |t|
