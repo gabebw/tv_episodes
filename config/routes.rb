437aba20c7610cc9ea6fae189a17420309d0b2d3
Gabe Berke-Williams <gbw@brandeis.edu>
2010-06-15 08:41:24 -0400

    Oops: userd wrong controller name

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
-  map.login "login", :controller => "users_sessions_controller", :action => "new"
-  map.logout "logout", :controller => "users_sessions_controller", :action => "destroy"
+  map.login "login", :controller => "user_sessions", :action => "new"
+  map.logout "logout", :controller => "user_sessions", :action => "destroy"
 
   map.resources :user_sessions
 
