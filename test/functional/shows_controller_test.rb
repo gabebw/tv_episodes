0d0466ba69d6a87de694b4bbe00a56fe1ce3f8da
Gabe Berke-Williams <gbw@brandeis.edu>
2010-06-15 20:50:46 -0400

    Fixed up functional for shows and users; still not working :(

--- test/functional/shows_controller_test.rb
+++ test/functional/shows_controller_test.rb
@@ -14,30 +14,30 @@ class ShowsControllerTest < ActionController::TestCase
 
   test "should create show" do
     assert_difference('Show.count') do
-      post :create, :show => { }
+      post :create, :show => shows(:veronica_mars).attributes
     end
 
     assert_redirected_to show_path(assigns(:show))
   end
 
   test "should show show" do
-    get :show, :id => shows(:one).to_param
+    get :show, :id => shows(:veronica_mars).to_param
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, :id => shows(:one).to_param
+    get :edit, :id => shows(:veronica_mars).to_param
     assert_response :success
   end
 
   test "should update show" do
-    put :update, :id => shows(:one).to_param, :show => { }
+    put :update, :id => shows(:veronica_mars).to_param, :show => {:name => "New name"}
     assert_redirected_to show_path(assigns(:show))
   end
 
   test "should destroy show" do
     assert_difference('Show.count', -1) do
-      delete :destroy, :id => shows(:one).to_param
+      delete :destroy, :id => shows(:veronica_mars).to_param
     end
 
     assert_redirected_to shows_path
